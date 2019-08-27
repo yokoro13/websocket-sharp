b9fecae7a0117677d265925391f3d00a980f0cc4
yokoro13 <34597246+yokoro13@users.noreply.github.com>
2019-08-27 16:16:26 +0900

    CHANGE FragmentLength value

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -156,7 +156,7 @@ namespace WebSocketSharp
     {
       _maxRetryCountForConnect = 10;
       EmptyBytes = new byte[0];
-      FragmentLength = 1016;
+      FragmentLength = 262144;
       RandomNumber = new RNGCryptoServiceProvider ();
     }
 
